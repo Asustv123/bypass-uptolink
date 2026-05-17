442a9eaa8055d6d8099862808af9c43424a35318
Asustv123 <112798924+Asustv123@users.noreply.github.com>
2026-05-17 12:49:27 +0700

    Add UserScript for bypassing UptoLink
    Initial creation of a UserScript for bypassing UptoLink.

---
+++ Script.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Bypass UptoLink (GitHub)
+// @namespace    https://github.com/Asustv123
+// @version      1.0
+// @description  Auto bypass
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/Asustv123/bypass-uptolink/main/source.js
+// @grant        GM_xmlhttpRequest
+// @connect      uptolink.one
+// @connect      api.kolosal.ai
+// @run-at       document-end
+// ==/UserScript== 
